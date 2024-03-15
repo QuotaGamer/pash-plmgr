730ab7fbbaf7f20549f83e3c5a267614f0694fb9
QuotaGamer <78985957+QuotaGamer@users.noreply.github.com>
2024-03-15 07:13:27 -0500

    Up-db

--- valid_plugins.py
+++ valid_plugins.py
@@ -1,7 +1,9 @@
+ver:float=1.1
 _available_plugins={
     "plmgr.py": True,
     "exiter.py": True,
     "print.py": True,
     "shell.py": True,
-    "template.py": True
+    "template.py": True,
+    "valid_plugins.py": True
 }
